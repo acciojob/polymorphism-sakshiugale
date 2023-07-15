658d0dc10478c5b82466f2422f9bcc0548894da2
SAKSHI UGALE <sakshiugale452@gmail.com>
2023-07-15 19:39:19 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        int result1 = p.product(2, 3);
+        int result2 = p.product(2, 3, 4);
+        double result3 = p.product(2.5, 3.5);
 
+        System.out.println("Result 1: " + result1);
+        System.out.println("Result 2: " + result2);
+        System.out.println("Result 3: " + result3);
+    }
+
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
